c9dd393437071b2d072bdb91f32bb80dac43780d
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2016-01-07 20:59:02 -0600

    ASoC: Intel: bytcr_rt5640: enable AIF_SWAP for Asus T00TAF
    Swap the interfaces
    
    DO NOT SUBMIT UPSTREAM, should come from bytcr detection

--- sound/soc/intel/boards/bytcr_rt5640.c
+++ sound/soc/intel/boards/bytcr_rt5640.c
@@ -150,7 +150,9 @@ static const struct dmi_system_id byt_rt5640_quirk_table[] = {
 			DMI_EXACT_MATCH(DMI_SYS_VENDOR, "ASUSTeK COMPUTER INC."),
 			DMI_EXACT_MATCH(DMI_PRODUCT_NAME, "T100TAF"),
 		},
-		.driver_data = (unsigned long *)BYT_RT5640_IN1_MAP,
+		.driver_data = (unsigned long *)(BYT_RT5640_IN1_MAP |
+						 BYT_RT5640_SWAP_AIF
+						 ),
 	},
 	{
 		.callback = byt_rt5640_quirk_cb,
