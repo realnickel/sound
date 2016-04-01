c7c4036a2d78c69b545143e0f4080a603467c042
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2016-03-31 19:03:02 -0500

    ASoC: Intel: bytcr_rt5640: enable SSP2_AIF2 quirk on T100TAF
    Asus T100TAF needs this quirk

--- sound/soc/intel/boards/bytcr_rt5640.c
+++ sound/soc/intel/boards/bytcr_rt5640.c
@@ -164,7 +164,8 @@ static const struct dmi_system_id byt_rt5640_quirk_table[] = {
 		},
 		.driver_data = (unsigned long *)(BYT_RT5640_IN1_MAP |
 						 BYT_RT5640_MONO_SPEAKER |
-						 BYT_RT5640_DIFF_MIC
+						 BYT_RT5640_DIFF_MIC |
+						 BYT_RT5640_SSP2_AIF2
 						 ),
 	},
 	{
