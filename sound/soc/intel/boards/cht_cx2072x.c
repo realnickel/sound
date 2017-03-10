086780a0c344146c05a68353d06064ea29016ebc
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2017-03-10 08:50:23 -0600

    ASoC: Intel: clean-up cht-cx720x machine driver
    remove quirks and code to handle the clock (it's handled by the
    codec driver)
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/intel/boards/cht_cx2072x.c
+++ sound/soc/intel/boards/cht_cx2072x.c
@@ -23,11 +23,8 @@
 #include <linux/platform_device.h>
 #include <linux/acpi.h>
 #include <linux/device.h>
-#include <linux/dmi.h>
 #include <linux/slab.h>
-#include <asm/cpu_device_id.h>
 #include <asm/platform_sst_audio.h>
-#include <linux/clk.h>
 #include <sound/pcm.h>
 #include <sound/pcm_params.h>
 #include <sound/soc.h>
@@ -36,98 +33,15 @@
 #include "../atom/sst-atom-controls.h"
 #include "../common/sst-acpi.h"
 
-
-#define CHT_PLAT_CLK_3_HZ	19200000
-
-enum {
-	CHT_CX_DMIC_MAP,
-};
-
-#define CHT_CX_MAP(quirk)	((quirk) & 0xff)
-#define CHT_CX_MCLK_EN	BIT(16)
-
-
 struct cht_mc_private {
-	struct clk *mclk;
 	struct snd_soc_jack_gpio hp_gpio;
 };
-
-static unsigned long cht_cx_quirk = CHT_CX_MCLK_EN;
-
-#define BYT_CODEC_DAI1	"cx2072x-hifi"
-
-static inline struct snd_soc_dai *byt_get_codec_dai(struct snd_soc_card *card)
-{
-	struct snd_soc_pcm_runtime *rtd;
-
-	list_for_each_entry(rtd, &card->rtd_list, list) {
-		if (!strncmp(rtd->codec_dai->name, BYT_CODEC_DAI1,
-			     strlen(BYT_CODEC_DAI1)))
-			return rtd->codec_dai;
-	}
-	return NULL;
-}
-
-static int platform_clock_control(struct snd_soc_dapm_widget *w,
-				  struct snd_kcontrol *k, int  event)
-{
-	struct snd_soc_dapm_context *dapm = w->dapm;
-	struct snd_soc_card *card = dapm->card;
-	struct snd_soc_dai *codec_dai;
-	struct cht_mc_private *priv = snd_soc_card_get_drvdata(card);
-	int ret;
-
-	codec_dai = byt_get_codec_dai(card);
-	if (!codec_dai) {
-		dev_err(card->dev,
-			"Codec dai not found; Unable to set platform clock\n");
-		return -EIO;
-	}
-
-	if (SND_SOC_DAPM_EVENT_ON(event)) {
-		if ((cht_cx_quirk & CHT_CX_MCLK_EN) && priv->mclk) {
-			ret = clk_prepare_enable(priv->mclk);
-			if (ret < 0) {
-				dev_err(card->dev,
-					"could not configure MCLK state");
-				return ret;
-			}
-		}
-		ret = snd_soc_dai_set_sysclk(codec_dai, CX2072X_MCLK_EXTERNAL_PLL,
-				     19200000, SND_SOC_CLOCK_IN);
-	} else {
-		/*
-		 * Set codec clock source to internal clock before
-		 * turning off the platform clock. Codec needs clock
-		 * for Jack detection and button press
-		 */
-
-		/* FIXME: what is the internal setting, if any? */
-		ret = snd_soc_dai_set_sysclk(codec_dai, CX2072X_MCLK_EXTERNAL_PLL,
-				     19200000, SND_SOC_CLOCK_IN);
-
-		if (!ret) {
-			if ((cht_cx_quirk & CHT_CX_MCLK_EN) && priv->mclk)
-				clk_disable_unprepare(priv->mclk);
-		}
-	}
-
-	if (ret < 0) {
-		dev_err(card->dev, "can't set codec sysclk: %d\n", ret);
-		return ret;
-	}
-
-	return 0;
-}
 
 static const struct snd_soc_dapm_widget cht_dapm_widgets[] = {
 	SND_SOC_DAPM_HP("Headphone", NULL),
 	SND_SOC_DAPM_MIC("Headset Mic", NULL),
 	SND_SOC_DAPM_MIC("Int Mic", NULL),
 	SND_SOC_DAPM_SPK("Ext Spk", NULL),
-	SND_SOC_DAPM_SUPPLY("Platform Clock", SND_SOC_NOPM, 0, 0,
-			platform_clock_control, SND_SOC_DAPM_PRE_PMU |
-			SND_SOC_DAPM_POST_PMD),
 };
 
 static const struct snd_soc_dapm_route cht_audio_map[] = {
@@ -152,8 +66,6 @@ static const struct snd_soc_dapm_route cht_audio_map[] = {
 		{"ssp2 Rx", NULL, "Capture"},
 		{"ssp0 Tx", NULL, "modem_out"},
 		{"modem_in", NULL, "ssp0 Rx"},
-		{"Playback", NULL, "Platform Clock"},
-		{"Capture", NULL, "Platform Clock"},
 };
 
 static const struct snd_kcontrol_new cht_mc_controls[] = {
@@ -166,28 +78,6 @@ static const struct snd_kcontrol_new cht_mc_controls[] = {
 static int cht_aif1_hw_params(struct snd_pcm_substream *substream,
 			     struct snd_pcm_hw_params *params)
 {
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
-	struct snd_soc_dai *codec_dai = rtd->codec_dai;
-	int ret;
-	
-	ret = snd_soc_dai_set_sysclk(codec_dai, CX2072X_MCLK_EXTERNAL_PLL,
-				     19200000, SND_SOC_CLOCK_IN);
-	if (ret < 0) {
-		dev_err(rtd->dev, "can't set codec sysclk: %d\n", ret);
-		return ret;
-	}
-
-	/* FIXME: No need PLL for conexant codec */
-#if 0
-	ret = snd_soc_dai_set_pll(codec_dai, 0, RT5670_PLL1_S_MCLK,
-				  CHT_PLAT_CLK_3_HZ, rate * 512);
-	if (ret < 0) {
-		dev_err(rtd->dev, "can't set codec pll: %d\n", ret);
-		return ret;
-	}
-#endif
-
-
 	return 0;
 }
 
@@ -212,13 +102,22 @@ static int cht_cx_jack_status_check(void *data)
 
 static int cht_codec_init(struct snd_soc_pcm_runtime *runtime)
 {
-	int ret=0;
+	int ret;
 	struct snd_soc_card *card = runtime->card;
 	struct cht_mc_private *priv = snd_soc_card_get_drvdata(runtime->card);
 	struct snd_soc_dai *codec_dai = runtime->codec_dai;
 	struct snd_soc_codec *codec = codec_dai->codec;
 
 	card->dapm.idle_bias_off = true;
+
+	/* set the default PLL rate, the clock is handled by the codec driver */
+	ret = snd_soc_dai_set_sysclk(codec_dai, CX2072X_MCLK_EXTERNAL_PLL,
+				     19200000, SND_SOC_CLOCK_IN);
+	if (ret) {
+		dev_err(codec->dev, "Could not set sysclk\n");
+		return ret;
+	}
+
 
 	/* FIXME: is this necessary */
 //      snd_soc_dapm_ignore_suspend(&codec->dapm, "PortD");
@@ -228,27 +127,6 @@ static int cht_codec_init(struct snd_soc_pcm_runtime *runtime)
 //	snd_soc_dapm_enable_pin(&card->dapm, "Int Mic");
 
 //	snd_soc_dapm_sync(&card->dapm);
-
-	if ((cht_cx_quirk & CHT_CX_MCLK_EN) && priv->mclk) {
-		/*
-		 * The firmware might enable the clock at
-		 * boot (this information may or may not
-		 * be reflected in the enable clock register).
-		 * To change the rate we must disable the clock
-		 * first to cover these cases. Due to common
-		 * clock framework restrictions that do not allow
-		 * to disable a clock that has not been enabled,
-		 * we need to enable the clock first.
-		 */
-		ret = clk_prepare_enable(priv->mclk);
-		if (!ret)
-			clk_disable_unprepare(priv->mclk);
-
-		ret = clk_set_rate(priv->mclk, 19200000);
-
-		if (ret)
-			dev_err(card->dev, "unable to set MCLK rate\n");
-	}
 
 	ret = snd_soc_card_jack_new(runtime->card, "Headset",
 				    SND_JACK_HEADSET | SND_JACK_BTN_0 |
@@ -418,18 +296,6 @@ static struct snd_soc_card chtcx2072x_card = {
 
 static char cht_cx_codec_name[16]; /* i2c-<HID>:00 with HID being 8 chars */
 
-static bool is_valleyview(void)
-{
-	static const struct x86_cpu_id cpu_ids[] = {
-		{ X86_VENDOR_INTEL, 6, 55 }, /* Valleyview, Bay Trail */
-		{}
-	};
-
-	if (!x86_match_cpu(cpu_ids))
-		return false;
-	return true;
-}
-
 static int snd_cht_mc_probe(struct platform_device *pdev)
 {
 	int ret_val = 0;
@@ -466,17 +332,6 @@ static int snd_cht_mc_probe(struct platform_device *pdev)
 			"%s%s", "i2c-", i2c_name);
 
 		cht_dailink[dai_index].codec_name = cht_cx_codec_name;
-	}
-
-	if ((cht_cx_quirk & CHT_CX_MCLK_EN) && (is_valleyview())) {
-		drv->mclk = devm_clk_get(&pdev->dev, "pmc_plt_clk_3");
-		if (IS_ERR(drv->mclk)) {
-			dev_err(&pdev->dev,
-				"Failed to get MCLK from pmc_plt_clk_3: %ld\n",
-				PTR_ERR(drv->mclk));
-			
-			return PTR_ERR(drv->mclk);
-		}
 	}
 
 	ret_val = devm_snd_soc_register_card(&pdev->dev, &chtcx2072x_card);
