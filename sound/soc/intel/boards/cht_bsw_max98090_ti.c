b25ac4be7a8962439bbcd72e0ee4b047f923ae03
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2017-09-08 00:13:05 -0500

    ASoC: Intel: cht_bsw_max98090: add support for Baytrail
    Distributions such as Fedora, Ubuntu and Gallium don't currently
    have a means to support Baytrail Chromebooks and other platforms
    with the same build [1][2] due to incompatible platform drivers.
    
    Add MCLK management to reuse this machine driver for Baytrail
    platforms and solve this coexistence problem at last. UCM files are
    provided at [3] and will eventually be submitted to the new repo.
    
    The legacy byt-max98090 machine driver is still maintained but can
    only be used when the other Atom/DPCM driver is not compiled in, or
    when users don't want to configure extra mixers required by the
    Atom/sst driver.
    
    Tested on Lenovo 100s Baytrail Chromebook w/ Mr. Chromebox BOOT_STUB
    firmware and Acer R11 Cherrytrail Chromebook
    
    [1] https://bugzilla.redhat.com/show_bug.cgi?id=1335196
    [2] http://mailman.alsa-project.org/pipermail/alsa-devel/2016-August/
    111641.html
    [3] https://github.com/plbossart/UCM/tree/master/byt-max98090
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Acked-By: Vinod Koul <vinod.koul@intel.com>
    Signed-off-by: Mark Brown <broonie@kernel.org>
    (cherry picked from commit 299bad365b900223cded22a6f66dbb3763da4235)

--- sound/soc/intel/boards/cht_bsw_max98090_ti.c
+++ sound/soc/intel/boards/cht_bsw_max98090_ti.c
@@ -41,6 +41,18 @@ struct cht_mc_private {
 	bool ts3a227e_present;
 };
 
+static inline struct snd_soc_dai *cht_get_codec_dai(struct snd_soc_card *card)
+{
+	struct snd_soc_pcm_runtime *rtd;
+
+	list_for_each_entry(rtd, &card->rtd_list, list) {
+		if (!strncmp(rtd->codec_dai->name, CHT_CODEC_DAI,
+			     strlen(CHT_CODEC_DAI)))
+			return rtd->codec_dai;
+	}
+	return NULL;
+}
+
 static int platform_clock_control(struct snd_soc_dapm_widget *w,
 					  struct snd_kcontrol *k, int  event)
 {
@@ -50,7 +62,7 @@ static int platform_clock_control(struct snd_soc_dapm_widget *w,
 	struct cht_mc_private *ctx = snd_soc_card_get_drvdata(card);
 	int ret;
 
-	codec_dai = snd_soc_card_get_codec_dai(card, CHT_CODEC_DAI);
+	codec_dai = cht_get_codec_dai(card);
 	if (!codec_dai) {
 		dev_err(card->dev, "Codec dai not found; Unable to set platform clock\n");
 		return -EIO;
@@ -218,6 +230,25 @@ static int cht_codec_init(struct snd_soc_pcm_runtime *runtime)
 		dev_err(runtime->dev,
 			"jack detection gpios not added, error %d\n", ret);
 	}
+
+	/*
+	 * The firmware might enable the clock at
+	 * boot (this information may or may not
+	 * be reflected in the enable clock register).
+	 * To change the rate we must disable the clock
+	 * first to cover these cases. Due to common
+	 * clock framework restrictions that do not allow
+	 * to disable a clock that has not been enabled,
+	 * we need to enable the clock first.
+	 */
+	ret = clk_prepare_enable(ctx->mclk);
+	if (!ret)
+		clk_disable_unprepare(ctx->mclk);
+
+	ret = clk_set_rate(ctx->mclk, CHT_PLAT_CLK_3_HZ);
+
+	if (ret)
+		dev_err(runtime->dev, "unable to set MCLK rate\n");
 
 	/*
 	 * The firmware might enable the clock at
