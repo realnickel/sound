kernelorg-mirror/linux_kernel_git_lrg_asoc

ASoC: SOF: CNL use correct FW and topology file names
Signed-off-by: Liam Girdwood <liam.r.girdwood@linux.intel.com>

sound/soc/sof/sof-pci-dev.c
@@ -269,8 +269,8 @@ static const struct sof_dev_desc byt_desc = {
 
 #if IS_ENABLED(CONFIG_SND_SOC_SOF_CANNONLAKE)
 static const struct snd_sof_machine sof_cnl_machines[] = {
-	{ "INT34C2", "cnl_alc700_i2s", "intel/dsp_fw_cnl.bin",
-		"intel/sof-cnl.tplg", "0000:00:0e.0", &snd_sof_cnl_ops },
+	{ "INT34C2", "cnl_alc700_i2s", "intel/reef-cnl.ri",
+		"intel/reef-cnl.tplg", "0000:00:0e.0", &snd_sof_cnl_ops },
 };
 
 static const struct sof_dev_desc cnl_desc = {
@@ -280,8 +280,8 @@ static const struct sof_dev_desc cnl_desc = {
 	.resindex_imr_base	= -1,
 	.irqindex_host_ipc	= -1,
 	.resindex_dma_base	= -1,
-	.nocodec_fw_filename = "intel/dsp_fw_cnl.bin",
-	.nocodec_tplg_filename = "intel/sof-cnl.tplg"
+	.nocodec_fw_filename = "intel/reef-cnl.ri",
+	.nocodec_tplg_filename = "intel/reef-cnl.tplg"
 };
 #endif
 

realnickel/sound

Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

sound/soc/sof/sof-pci-dev.c
@@ -127,8 +127,8 @@ static struct snd_soc_acpi_mach sof_cnl_machines[] = {
 	{
 		.id = "INT34C2",
 		.drv_name = "cnl_alc700_i2s",
-		.sof_fw_filename = "intel/dsp_fw_cnl.bin",
-		.sof_tplg_filename = "intel/sof-cnl.tplg",
+		.sof_fw_filename = "intel/reef-cnl.bin",
+		.sof_tplg_filename = "intel/reef-cnl.tplg",
 		.asoc_plat_name = "0000:00:0e.0",
 	},
 	{}
@@ -141,8 +141,8 @@ static const struct sof_dev_desc cnl_desc = {
 	.resindex_imr_base	= -1,
 	.irqindex_host_ipc	= -1,
 	.resindex_dma_base	= -1,
-	.nocodec_fw_filename = "intel/dsp_fw_cnl.bin",
-	.nocodec_tplg_filename = "intel/sof-cnl.tplg"
+	.nocodec_fw_filename = "intel/reef-cnl.bin",
+	.nocodec_tplg_filename = "intel/reef-cnl.tplg"
 };
 #endif
 