112444e90c4ae7e24d30262a75803f3c4ec09ff9
Yan Wang <yan.wang@linux.intel.com>
2018-02-05 23:17:37 +0800

    ASoC: Add snd_sof_dai structure for saving DAI and link information.
    This structure will be saved in snd_sof_dev structure for lookup.
    
    Signed-off-by: Yan Wang <yan.wang@linux.intel.com>

--- sound/soc/sof/core.c
+++ sound/soc/sof/core.c
@@ -130,6 +130,7 @@ static int sof_probe(struct platform_device *pdev)
 	INIT_LIST_HEAD(&sdev->pcm_list);
 	INIT_LIST_HEAD(&sdev->kcontrol_list);
 	INIT_LIST_HEAD(&sdev->widget_list);
+	INIT_LIST_HEAD(&sdev->dai_list);
 	dev_set_drvdata(&pdev->dev, sdev);
 	spin_lock_init(&sdev->ipc_lock);
 	spin_lock_init(&sdev->hw_lock);
