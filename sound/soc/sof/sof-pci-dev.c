84ed2d3ba0220424dd441f3a28ed2fe83967ad3a
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2018-03-08 15:44:09 -0600

    [SQUASHME] ASoC: SOF: pci: only use NOCODEC if enabled
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/sof/sof-pci-dev.c
+++ sound/soc/sof/sof-pci-dev.c
@@ -201,6 +201,7 @@ static int sof_pci_probe(struct pci_dev *pci,
 	/* find machine */
 	mach = snd_soc_acpi_find_machine(desc->machines);
 	if (!mach) {
+#if IS_ENABLED(CONFIG_SND_SOC_SOF_NOCODEC)
 		struct snd_soc_acpi_mach *m;
 
 		dev_err(dev, "No matching ASoC machine driver found - using nocodec\n");
@@ -214,6 +215,10 @@ static int sof_pci_probe(struct pci_dev *pci,
 		m->sof_tplg_filename = desc->nocodec_tplg_filename;
 		m->asoc_plat_name = "sof-platform";
 		mach = m;
+#else
+		dev_err(dev, "No matching ASoC machine driver found - aborting probe\n");
+		return -ENODEV;
+#endif
 	}
 
 	mach->pdata = ops;
