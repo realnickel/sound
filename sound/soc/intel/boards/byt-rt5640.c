f9214e4ad48d5b634635dc76bd07c08e2b8c6b72
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-01-17 21:24:47 -0600

    ASoC: Intel: byt-rt5640: use modern codec representation in dailinks
    Use .codecs and .num_codecs instead of .codec_name and .codec_dai_name
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/intel/boards/byt-rt5640.c
+++ sound/soc/intel/boards/byt-rt5640.c
@@ -180,13 +180,20 @@ static struct snd_soc_ops byt_rt5640_ops = {
 	.hw_params = byt_rt5640_hw_params,
 };
 
+static struct snd_soc_dai_link_component rt5640_component[] = {
+	{
+		.name = "i2c-10EC5640:00",
+		.dai_name = "rt5640-aif1"
+	}
+};
+
 static struct snd_soc_dai_link byt_rt5640_dais[] = {
 	{
 		.name = "Baytrail Audio",
 		.stream_name = "Audio",
 		.cpu_dai_name = "baytrail-pcm-audio",
-		.codec_dai_name = "rt5640-aif1",
-		.codec_name = "i2c-10EC5640:00",
+		.codecs = rt5640_component,
+		.num_codecs = ARRAY_SIZE(rt5640_component),
 		.platform_name = "baytrail-pcm-audio",
 		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
 			   SND_SOC_DAIFMT_CBS_CFS,
