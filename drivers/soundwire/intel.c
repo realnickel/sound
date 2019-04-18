be20d4ffe90df065bc8e9a5b16bc6a8c3283e7e1
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-04-18 16:27:21 -0500

    [HACK] soundwire: logs to debug sublink properties not read correctly
    This needs cleanup but helped show the problem with nested _DSD
    properties with references
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel.c
+++ drivers/soundwire/intel.c
@@ -952,6 +952,7 @@ static int intel_probe(struct platform_device *pdev)
 	sdw->cdns.msg_count = 0;
 	sdw->cdns.bus.dev = &pdev->dev;
 	sdw->cdns.bus.link_id = pdev->id;
+	sdw->cdns.bus.ctlr_fwnode = pdev->dev.fwnode;
 
 	sdw_cdns_probe(&sdw->cdns);
 
@@ -967,6 +968,7 @@ static int intel_probe(struct platform_device *pdev)
 		goto err_master_reg;
 	}
 
+#if 0
 	/* Initialize shim and controller */
 	intel_link_power_up(sdw);
 	intel_shim_init(sdw);
@@ -1003,6 +1005,7 @@ static int intel_probe(struct platform_device *pdev)
 	}
 
 	intel_debugfs_init(sdw);
+#endif
 
 	return 0;
 
