cedd9858dacc78093a133005907ffa5dd273131b
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-07-18 13:50:53 -0500

    soundwire: cadence_master: use BIOS defaults for frame shape
    Remove hard-coding and use BIOS values. If they are wrong use default
    48x2 frame shape.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/cadence_master.c
+++ drivers/soundwire/cadence_master.c
@@ -198,9 +198,6 @@ MODULE_PARM_DESC(cdns_mcp_int_mask, "Cadence MCP IntMask");
 #define CDNS_PDI_CONFIG_PORT			GENMASK(4, 0)
 
 /* Driver defaults */
-//#define CDNS_DEFAULT_FRAME_SHAPE		0x30 /* 125 rows, 2 columns */
-//#define CDNS_DEFAULT_FRAME_SHAPE		0x08 /* 50 rows, 2 columns */
-#define CDNS_DEFAULT_FRAME_SHAPE		0x09 /* 50 rows, 4 columns */
 #define CDNS_DEFAULT_SSP_INTERVAL		0x18
 #define CDNS_TX_TIMEOUT				2000
 
@@ -1236,6 +1233,20 @@ int sdw_cdns_pdi_init(struct sdw_cdns *cdns,
 }
 EXPORT_SYMBOL(sdw_cdns_pdi_init);
 
+static u32 cdns_set_default_frame_shape(int n_rows, int n_cols)
+{
+	u32 val;
+	int c;
+	int r;
+
+	r = sdw_find_row_index(n_rows);
+	c = sdw_find_col_index(n_cols);
+
+	val = (r << 3) | c;
+
+	return val;
+}
+
 /**
  * sdw_cdns_init() - Cadence initialization
  * @cdns: Cadence instance
@@ -1270,7 +1281,9 @@ int sdw_cdns_init(struct sdw_cdns *cdns)
 	       val);
 
 	/* Set the default frame shape */
-	cdns_writel(cdns, CDNS_MCP_FRAME_SHAPE_INIT, CDNS_DEFAULT_FRAME_SHAPE);
+	val = cdns_set_default_frame_shape(prop->default_row,
+					   prop->default_col);
+	cdns_writel(cdns, CDNS_MCP_FRAME_SHAPE_INIT, val);
 
 	/* Set SSP interval to default value */
 	cdns_writel(cdns, CDNS_MCP_SSP_CTRL0, CDNS_DEFAULT_SSP_INTERVAL);
