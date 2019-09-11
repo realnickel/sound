2b06635f9c2516d6bc023c48341dfa629e3a867e
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-09-10 19:23:22 -0500

    soundwire: add initial definitions for sdw_master_device
    Since we want an explicit support for the SoundWire Master device, add
    the definitions, following the Grey Bus example.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- include/linux/soundwire/sdw_type.h
+++ include/linux/soundwire/sdw_type.h
@@ -6,10 +6,16 @@
 
 extern struct bus_type sdw_bus_type;
 extern struct device_type sdw_slave_type;
+extern struct device_type sdw_md_type;
 
 static inline int is_sdw_slave(const struct device *dev)
 {
 	return dev->type == &sdw_slave_type;
+}
+
+static inline int is_sdw_md(const struct device *dev)
+{
+	return dev->type == &sdw_md_type;
 }
 
 #define to_sdw_slave_driver(_drv) \
@@ -25,13 +31,6 @@ void sdw_unregister_slave_driver(struct sdw_slave_driver *drv);
 #define drv_to_sdw_md_driver(_drv) \
 	container_of(_drv, struct sdw_md_driver, driver)
 
-#define sdw_register_md_driver(drv) \
-	__sdw_register_md_driver(drv, THIS_MODULE)
-
-int __sdw_register_md_driver(struct sdw_md_driver *drv,
-			     struct module *owner);
-void sdw_unregister_md_driver(struct sdw_md_driver *drv);
-
 int sdw_slave_modalias(const struct sdw_slave *slave, char *buf, size_t size);
 
 /**
@@ -45,18 +44,5 @@ int sdw_slave_modalias(const struct sdw_slave *slave, char *buf, size_t size);
 #define module_sdw_slave_driver(__sdw_slave_driver) \
 	module_driver(__sdw_slave_driver, sdw_register_slave_driver, \
 			sdw_unregister_slave_driver)
-
-/**
- * module_sdw_md_driver() - Helper macro for registering a Soundwire driver
- * @__sdw_md_driver: soundwire md (Master Device) driver struct
- *
- * Helper macro for Soundwire drivers which do not do anything special in
- * module init/exit. This eliminates a lot of boilerplate. Each module may only
- * use this macro once, and calling it replaces module_init() and module_exit()
- */
-#define module_sdw_md_driver(__sdw_md_driver) \
-	module_driver(__sdw_md_driver, sdw_register_md_driver, \
-			sdw_unregister_md_driver)
-
 
 #endif /* __SOUNDWIRE_TYPES_H */
