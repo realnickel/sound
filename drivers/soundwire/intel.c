093ab46c6b48d0383292d713c04d4e7c547791f9
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2019-12-05 13:51:49 -0600

    soundwire: intel: add CLK_STOP_NOT_ALLOWED support
    In case the clock needs to keep running, we need to prevent the Master
    from entering pm_runtime suspend.
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel.c
+++ drivers/soundwire/intel.c
@@ -1359,6 +1359,7 @@ static int intel_master_startup(struct sdw_bus *bus)
 	struct sdw_cdns_stream_config config;
 	struct sdw_intel *sdw;
 	int link_flags;
+	u32 clock_stop_quirks;
 	int ret;
 
 	if (!bus)
@@ -1420,6 +1421,20 @@ static int intel_master_startup(struct sdw_bus *bus)
 		pm_runtime_enable(&bus->dev);
 	}
 
+	clock_stop_quirks = sdw->link_res->clock_stop_quirks;
+	if (clock_stop_quirks & SDW_INTEL_CLK_STOP_NOT_ALLOWED) {
+		/*
+		 * To keep the clock running we need to prevent
+		 * pm_runtime suspend from happening by increasing the
+		 * reference count.
+		 * This quirk is specified by the parent PCI device in
+		 * case of specific latency requirements. It will have
+		 * no effect if pm_runtime is disabled by the user via
+		 * a module parameter for testing purposes.
+		 */
+		pm_runtime_get_noresume(&bus->dev);
+	}
+
 	/*
 	 * The runtime PM status of Slave devices is "Unsupported"
 	 * until they report as ATTACHED. If they don't, e.g. because
@@ -1454,6 +1469,12 @@ static int intel_master_delete(struct sdw_bus *bus)
 	pm_runtime_disable(&bus->dev);
 
 	sdw = bus->pdata;
+
+	/*
+	 * Since pm_runtime is already disabled, we don't decrease
+	 * the refcount when the clock_stop_quirk is
+	 * SDW_INTEL_CLK_STOP_NOT_ALLOWED
+	 */
 
 	if (!sdw->cdns.bus.prop.hw_disabled) {
 		intel_debugfs_exit(sdw);
