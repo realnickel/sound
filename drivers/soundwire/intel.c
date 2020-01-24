5926d9dd13c4a65d297bb46aab784510438768ca
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-01-23 18:33:06 -0600

    soundwire: intel: add multi-link hw_synchronization information
    set the flags as required by hardware implementation
    
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- drivers/soundwire/intel.c
+++ drivers/soundwire/intel.c
@@ -1388,7 +1388,14 @@ static int intel_master_startup(struct sdw_bus *bus)
 		dev_dbg(&bus->dev, "Multi-link is disabled\n");
 		bus->multi_link = false;
 	} else {
+		/*
+		 * hardware-based synchronization is required regardless
+		 * of the number of segments used by a stream: SSP-based
+		 * synchronization is gated by gsync when the multi-master
+		 * mode is set.
+		 */
 		bus->multi_link = true;
+		bus->hw_sync_min_links = 1;
 	}
 
 	/* Initialize shim, controller */
