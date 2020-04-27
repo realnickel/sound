b0be925eb350f7741b32faba100d0d018870797b
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2020-04-27 17:27:46 -0500

    ASoC: soc-pcm: dpcm: fix playback/capture checks
    Recent changes in the ASoC core prevent multi-cpu BE dailinks from
    being used. DPCM does support multi-cpu DAIs for BE Dailinks, but not
    for FE.
    
    Handle the FE checks first, and make sure all DAIs support the same
    capabilities within the same dailink.
    
    BugLink: https://github.com/thesofproject/linux/issues/2031
    Fixes: 9b5db059366ae2 ("ASoC: soc-pcm: dpcm: Only allow playback/capture if supported")
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>

--- sound/soc/soc-pcm.c
+++ sound/soc/soc-pcm.c
@@ -2908,20 +2908,55 @@ int soc_new_pcm(struct snd_soc_pcm_runtime *rtd, int num)
 	struct snd_pcm *pcm;
 	char new_name[64];
 	int ret = 0, playback = 0, capture = 0;
+	int invalid_dais;
+	int stream;
 	int i;
 
+
+	if (rtd->dai_link->dynamic && rtd->num_cpus > 1) {
+		dev_err(rtd->dev,
+			"DPCM doesn't support Multi CPU for Front-Ends yet\n");
+		return -EINVAL;
+	}
+
 	if (rtd->dai_link->dynamic || rtd->dai_link->no_pcm) {
-		cpu_dai = asoc_rtd_to_cpu(rtd, 0);
-		if (rtd->num_cpus > 1) {
-			dev_err(rtd->dev,
-				"DPCM doesn't support Multi CPU yet\n");
-			return -EINVAL;
+		if (rtd->dai_link->dpcm_playback) {
+			invalid_dais = 0;
+			stream = SNDRV_PCM_STREAM_PLAYBACK;
+
+			for_each_rtd_cpu_dais(rtd, i, cpu_dai)
+				if (!snd_soc_dai_stream_valid(cpu_dai, stream))
+					invalid_dais++;
+
+			if (!invalid_dais) {
+				playback = 1;
+			} else {
+				dev_err(rtd->card->dev,
+					"%d invalid CPU DAI(s) for rtd %s without playback support\n",
+					invalid_dais,
+					rtd->dai_link->stream_name);
+				return -EINVAL;
+			}
 		}
 
-		playback = rtd->dai_link->dpcm_playback &&
-			   snd_soc_dai_stream_valid(cpu_dai, SNDRV_PCM_STREAM_PLAYBACK);
-		capture = rtd->dai_link->dpcm_capture &&
-			  snd_soc_dai_stream_valid(cpu_dai, SNDRV_PCM_STREAM_CAPTURE);
+		if (rtd->dai_link->dpcm_capture) {
+			invalid_dais = 0;
+			stream = SNDRV_PCM_STREAM_CAPTURE;
+
+			for_each_rtd_cpu_dais(rtd, i, cpu_dai)
+				if (!snd_soc_dai_stream_valid(cpu_dai, stream))
+					invalid_dais++;
+
+			if (!invalid_dais) {
+				capture = 1;
+			} else {
+				dev_err(rtd->card->dev,
+					"%d invalid CPU DAI(s) for rtd %s without capture support\n",
+					invalid_dais,
+					rtd->dai_link->stream_name);
+				return -EINVAL;
+			}
+		}
 	} else {
 		/* Adapt stream for codec2codec links */
 		int cpu_capture = rtd->dai_link->params ?
